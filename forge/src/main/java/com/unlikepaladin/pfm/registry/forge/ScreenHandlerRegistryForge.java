ae99212a1a2459770fc21b61afa93a1b53836ec9
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2022-09-16 23:42:46 -0500

    Added a Trashcan

--- forge/src/main/java/com/unlikepaladin/pfm/registry/forge/ScreenHandlerRegistryForge.java
+++ forge/src/main/java/com/unlikepaladin/pfm/registry/forge/ScreenHandlerRegistryForge.java
@@ -25,13 +25,15 @@ public class ScreenHandlerRegistryForge {
         ScreenHandlerIDs.STOVE_SCREEN_HANDLER = IForgeMenuType.create(StoveScreenHandler::new);
         ScreenHandlerIDs.IRON_STOVE_SCREEN_HANDLER = IForgeMenuType.create(IronStoveScreenHandler::new);
         ScreenHandlerIDs.MICROWAVE_SCREEN_HANDLER = IForgeMenuType.create(MicrowaveScreenHandler::new);
+        ScreenHandlerIDs.TRASHCAN_SCREEN_HANDLER = IForgeMenuType.create(TrashcanScreenHandler::new);
 
         event.getRegistry().registerAll(
                 ScreenHandlerIDs.WORKBENCH_SCREEN_HANDLER.setRegistryName("furniture"),
                 ScreenHandlerIDs.STOVE_SCREEN_HANDLER.setRegistryName("stove_block_entity"),
                 ScreenHandlerIDs.IRON_STOVE_SCREEN_HANDLER.setRegistryName("iron_stove_block_entity"),
                 ScreenHandlerIDs.MICROWAVE_SCREEN_HANDLER .setRegistryName("microwave_block_entity"),
-                ScreenHandlerIDs.FREEZER_SCREEN_HANDLER.setRegistryName("freezer_block_entity")
+                ScreenHandlerIDs.FREEZER_SCREEN_HANDLER.setRegistryName("freezer_block_entity"),
+                ScreenHandlerIDs.TRASHCAN_SCREEN_HANDLER.setRegistryName("trashcan_block_entity")
             );
 
     }
